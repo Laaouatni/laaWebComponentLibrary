9f3940049fa44c2e309047daf68d1ebc6141c0dd
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2024-12-29 18:15:51 +0100

    Update laaWebComponentLibrary.js with the version of the code I have written in the  https://github.com/Laaouatni/arduino-2024-2025-prove/tree/main/2025/svelteRecreation

--- laaWebComponentLibrary.js
+++ laaWebComponentLibrary.js
@@ -5,54 +5,65 @@ document.querySelectorAll("template").forEach((thisTemplateElement) => {
     /**
      * @type {ShadowRoot}
      */
-    shadow;
+    shadowDom;
+
+    /**
+     *
+     * @type {{[variableName:string]: any}}
+     */
+    stateVariables = {};
     constructor() {
       super();
-      this.shadow = this.attachShadow({ mode: "open" });
-      this.shadow.appendChild(thisTemplateElement.content.cloneNode(true));
-
-      const hasScriptTagInTemplateComponent =
-        thisTemplateElement.content.querySelectorAll("script[nomodule]");
-
-      hasScriptTagInTemplateComponent.forEach((thisTemplateComponentScriptNoModule) => {
-        const generatedShadowScript = document.createElement("script");
-        generatedShadowScript.textContent = isolateScriptStringInsideComponent(
-          thisTemplateComponentScriptNoModule.textContent || "",
-        );
-        this.shadow.host.appendChild(generatedShadowScript);
-        this.shadow.querySelectorAll("script[nomodule]").forEach((thisComponentScriptNoModule) => {
-          thisComponentScriptNoModule.remove();
-        });
-      });
     }
 
     static get observedAttributes() {
       const attributesToObserve = [...thisTemplateElement.attributes]
         .map((thisAttribute) => thisAttribute.nodeName)
         .filter((thisAttribute) => {
-          const isIdAttribute = thisAttribute === "id";
-          if (isIdAttribute) return;
-          return true;
+          let canSkip = false;
+          ["id", "class", "style"].forEach((thisAttributeName) => {
+            if (thisAttribute == thisAttributeName) canSkip = true;
+          });
+          return !canSkip;
         });
       return attributesToObserve;
     }
 
     _connectedCallback() {}
+    connectedCallback() {
+      this._connectedCallback();
+
+      const copyFromTemplateToComponent = {
+        templateContent: copyTemplateContentToComponentShadowDom.bind(this),
+        attributes: copyAttributesFromTemplateToComponent.bind(this),
+        scripts: copyScriptsFromTemplateToComponent.bind(this),
+      };
+
+      copyFromTemplateToComponent.templateContent();
+      copyFromTemplateToComponent.attributes(["class", "style"]);
+
+      this.stateVariables = new Proxy(this.stateVariables, {
+        set: (parent, child, val) => {
+          parent[child] = val;
+          updateComponentInnerHtmlVariables(this);
+          return true;
+        },
+      });
+
+      copyFromTemplateToComponent.scripts();
+    }
+
     _disconnectedCallback() {}
+    disconnectedCallback() {
+      this._disconnectedCallback();
+    }
+
     /**
      * @param {string} attributeName
      * @param {any} oldValue
      * @param {any} newValue
      */
     _attributeChangedCallback(attributeName, oldValue, newValue) {}
-
-    connectedCallback() {
-      this._connectedCallback();
-    }
-
-    disconnectedCallback() {
-      this._disconnectedCallback();
-    }
     /**
      * @param {string} attributeName
      * @param {any} oldValue
@@ -62,16 +73,158 @@ document.querySelectorAll("template").forEach((thisTemplateElement) => {
       this._attributeChangedCallback(attributeName, oldValue, newValue);
     }
   }
-  customElements.define(thisTemplateElement.id, ThisComponent);
-});
 
-/**
- * @param   {string} thisScriptString
- * @returns {string}
- */
-function isolateScriptStringInsideComponent(thisScriptString) {
-  return `(()=>{
-    const thisComponent = document.currentScript.parentElement;
-    ${thisScriptString}}
-  )()`;
-}
+  customElements.define(thisTemplateElement.id, ThisComponent);
+
+  /**
+   *
+   * @param {ThisComponent} thisComponent
+   */
+  function updateComponentInnerHtmlVariables(thisComponent) {
+    thisComponent.shadowDom.innerHTML =
+      replaceHtmlStringVariablesBracketsWithValues(thisComponent);
+  }
+
+  /**
+   *
+   * @param {string} htmlStringToMinify
+   * @returns {string}
+   */
+  function minifyHtmlString(htmlStringToMinify) {
+    return htmlStringToMinify.replaceAll("\n", "").replaceAll("  ", "");
+  }
+
+  /**
+   *
+   * @param {ThisComponent} thisComponent
+   * @returns {string}
+   */
+  function replaceSlotTagWithSlotContent(thisComponent) {
+    const minifiedHtmlStrings = {
+      shadowDom: minifyHtmlString(thisTemplateElement.innerHTML),
+      slot: minifyHtmlString(thisComponent.innerHTML),
+    };
+    const regexScriptTag = /<script>.*<\/script>/g;
+    const slotHtmlStringWithoutScripts = minifiedHtmlStrings.slot.replace(
+      regexScriptTag,
+      "",
+    );
+    return minifiedHtmlStrings.shadowDom.replaceAll(
+      "<slot></slot>",
+      slotHtmlStringWithoutScripts,
+    );
+  }
+
+  /**
+   *
+   * @param {ThisComponent} thisComponent
+   * @returns {string}
+   */
+  function replaceHtmlStringVariablesBracketsWithValues(thisComponent) {
+    const thisComponentHtmlWithSlotTagReplacedWithSlotContent =
+      replaceSlotTagWithSlotContent(thisComponent);
+    const regexGetAllVariableBracketsInString = /\{.[^}]*\}/g;
+
+    return thisComponentHtmlWithSlotTagReplacedWithSlotContent.replace(
+      regexGetAllVariableBracketsInString,
+      (thisVariableBracketStringPart) => {
+        const variableName = thisVariableBracketStringPart.replace(
+          /\{|\}/g,
+          "",
+        );
+        return thisComponent.stateVariables[variableName];
+      },
+    );
+  }
+
+  function copyTemplateContentToComponentShadowDom() {
+    this.shadowDom = this.attachShadow({ mode: "open" });
+    const clonedTemplateContent = thisTemplateElement.content.cloneNode(true);
+
+    clonedTemplateContent.childNodes.forEach((thisChild) => {
+      if (thisChild instanceof HTMLScriptElement) thisChild.remove();
+    });
+
+    this.shadowDom.appendChild(clonedTemplateContent);
+  }
+
+  /**
+   *
+   * @param {string[]} attributesToCopyArray
+   */
+  function copyAttributesFromTemplateToComponent(attributesToCopyArray) {
+    attributesToCopyArray.forEach((thisAttributeName) => {
+      if (!thisTemplateElement.hasAttribute(thisAttributeName)) return;
+
+      this.setAttribute(
+        thisAttributeName,
+        `${
+          this.getAttribute(thisAttributeName) || ""
+        } ${thisTemplateElement.getAttribute(thisAttributeName)}`.trim(),
+      );
+    });
+  }
+
+  function copyScriptsFromTemplateToComponent() {
+    const allScriptElementsInsideTemplate =
+      thisTemplateElement.content.querySelectorAll("script");
+
+    const generatedScriptElementInsideComponent =
+      document.createElement("script");
+
+    const mergedScriptTags = [...allScriptElementsInsideTemplate]
+      .map((thisScriptTemplateElement) => {
+        if (!thisScriptTemplateElement.textContent) return "";
+        return addSyntacticSugarVariableDeclarationsToScriptTextContent(
+          thisScriptTemplateElement.textContent,
+        );
+      })
+      .join("");
+
+    generatedScriptElementInsideComponent.textContent =
+      isolateScriptStringInsideComponent(mergedScriptTags).replaceAll("  ", "");
+
+    this.appendChild(generatedScriptElementInsideComponent);
+  }
+
+  /**
+   * @param {string} scriptTextContentString
+   * @returns {string}
+   */
+  function addSyntacticSugarVariableDeclarationsToScriptTextContent(
+    scriptTextContentString,
+  ) {
+    // !TODO fix the thisComponent.stateVariables.q = q+1;
+    const STATE_OBJECT_POSITION_PREFIX_STRING = "thisComponent.stateVariables.";
+    const replaceVariableSettingWithPrefix = scriptTextContentString.replaceAll(
+      /.*=/g,
+      (thisString) => {
+        const regexs = {
+          variableDefinitionTypes: /let |const |var /g,
+          anonymousCallbackVoid: /\(.*\).*=/g,
+        };
+        const isCallback = thisString.match(regexs.anonymousCallbackVoid);
+        const isDefinition = thisString.match(regexs.variableDefinitionTypes);
+
+        if (isCallback) return thisString;
+        if (isDefinition)
+          return thisString.replace(
+            regexs.variableDefinitionTypes,
+            STATE_OBJECT_POSITION_PREFIX_STRING,
+          );
+        return `${STATE_OBJECT_POSITION_PREFIX_STRING}${thisString}`;
+      },
+    );
+    return replaceVariableSettingWithPrefix.replaceAll(" ", "");
+  }
+  /**
+   * @param   {string} thisScriptString
+   * @returns {string}
+   */
+  function isolateScriptStringInsideComponent(thisScriptString) {
+    return `(()=>{
+              const thisComponent = document.currentScript.parentElement;
+              ${thisScriptString}}
+            )()`;
+  }
+});
